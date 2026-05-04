fa150f4fc19346b27f01fc933b439c025ab2e7ab
gustavo-machado264 <britomachadogustavo@gmail.com>
2026-05-04 16:21:45 -0300

    integrado com supabase

---
+++ controle-gastos/controle-gastos/frontend/src/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>💰 Controle de Gastos funcionando!</Text>
+    </View>
+  );
+}
